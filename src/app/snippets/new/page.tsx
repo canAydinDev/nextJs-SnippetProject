c9cdbff325c6f84907a0ad29c69eb91f10ec232b
can aydin <canaydindev@gmail.com>
2024-02-22 14:05:17 +0300

    last update

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+
+import { useFormState } from "react-dom";
+import * as actions from '@/actions';
+
 export default function SnippetCreatePage() {
-  return <form>
+  const [formState, action] = useFormState(actions.createSnippet, {message: ''})
+  
+
+  return <form action={action}>
     <h3 className="font-bold m-3">Create a snippet</h3>
     <div className="flex flex-col gap-4">
       <div className="flex gap-4">
@@ -22,8 +30,14 @@ export default function SnippetCreatePage() {
           id="code"
         />
       </div>
+      {
+        formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null
+      }
       <button type="submit" className="border rounded p-2 bg-blue-200">
         Create
+      </button>
+      <button type="button" className="border rounded p-2 bg-blue-200">
+        Back
       </button>
     </div>
   </form>
